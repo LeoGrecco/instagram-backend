bdddc497f08382076f1643bc6b446c971bdbca6d
LeoGrecco <lgrecco1987@gmail.com>
2026-09-02 01:19:49 -0300

    Commit

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/health', (_req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
 app.use('/', setRoutes());
 
 app.listen(ENV.PORT, () => {
